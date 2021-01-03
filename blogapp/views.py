b3eef00f84281f5363aaa748f772fbf5e095f9d1
eunkyung <unmelted@naver.com>
2021-01-03 20:19:53 +0900

    post detail view add

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -9,3 +9,22 @@ class PostList(ListView):
 
     def get_queryset(self):
         return Post.objects.order_by('-created')
+
+
+
+
+class PostDetail(DetailView):
+    model = Post
+
+'''
+def post_detail(request, pk):
+    blog_post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blogapp/post_detail.html',
+        {
+            'blog_post': blog_post,
+        }
+    )
+'''
