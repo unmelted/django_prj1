d3c77788ab9c2c51f89565a34c323de3611e60db
eunkyung <unmelted@naver.com>
2021-01-07 22:54:54 +0900

    category add ongoing

--- blogapp/models.py
+++ blogapp/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(blank=True)
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -10,9 +14,11 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
 
     def get_absolute_url(self):
         return '/blog/{}/'.format(self.pk)
+
