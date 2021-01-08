a2d774c82ad003ac18834e4835a4f8f65318b77b
eunkyung <unmelted@naver.com>
2021-01-09 01:16:57 +0900

    category appear in list, detail

--- blogapp/models.py
+++ blogapp/models.py
@@ -7,6 +7,12 @@ class Category(models.Model):
     name = models.CharField(max_length=25, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
