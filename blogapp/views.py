c34644cde8fc74366eff5c47e7a3ef5ba51515b8
eunkyung <unmelted@naver.com>
2021-01-14 23:51:48 +0900

    search by tag

--- blogapp/views.py
+++ blogapp/views.py
@@ -57,6 +57,23 @@ class PostListByCategory(ListView):
         #context['title'] = 'Blog - {}'.format(category.name)
         return context
 
+class PostListByTag(ListView):
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+        tag = Tag.objects.get(slug=slug)
+
+        return tag.post_set.order_by('-created')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(type(self), self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['posts_without_category'] = Post.objects.filter(category=None).count()
+
+        slug = self.kwargs['slug']
+        context['tag'] = Tag.objects.get(slug=slug)
+
+        return context
+
 '''
 def post_detail(request, pk):
     blog_post = Post.objects.get(pk=pk)
