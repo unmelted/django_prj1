066066bc176e1a029cc98bb993e2e68cfe5b8efd
eunkyung <unmelted@naver.com>
2021-01-17 23:19:21 +0900

    commnet list show in post detail

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name', )}
@@ -8,5 +8,6 @@ class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
 admin.site.register(Post)
+admin.site.register(Comment)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
